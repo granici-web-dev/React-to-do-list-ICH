c738cc3d9f9de146d8bdd633b2415209f2160a51
Serghei_Granici <granici.design@gmail.com>
2026-02-16 13:32:47 +0100

    fixed bug

--- src/components/todoList/index.jsx
+++ src/components/todoList/index.jsx
@@ -1,15 +1,15 @@
-import { useContext } from "react";
-import TodoContext from "../../context/todoContext";
-import NoteItem from '../todoItem';
+import { useContext } from 'react';
+import TodoContext from '../../context/todoContext';
+import TodoItem from '../todoItem';
 
 function TodoList() {
-  const {notes} = useContext(TodoContext)
+  const { notes } = useContext(TodoContext);
 
   return (
     <div>
       <ul>
         {notes.map((note) => {
-          return <NoteItem key={note.id} {...note}/>;
+          return <TodoItem key={note.id} {...note} />;
         })}
       </ul>
     </div>
